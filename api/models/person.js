391dad9681e95ed4beda4bbb29ee35d85b991ec7
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-10 16:05:12 -0300

    [feat] adding email validation to People

--- api/models/person.js
+++ api/models/person.js
@@ -1,37 +1,46 @@
 'use strict';
 const {
-  Model
+   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Person extends Model {
-    static associate(models) {
-      Person.hasMany(models.Class, {
-        foreignKey: "professorId"
-      });
-      Person.hasMany(models.Registration, {
-        foreignKey: "studentId"
-      });
-    }
-  }
-  Person.init({
-    name: DataTypes.STRING,
-    active: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Person',
-    paranoid: true,
-    defaultScope: {
-      where: {
-        active: true
+   class Person extends Model {
+      static associate(models) {
+         Person.hasMany(models.Class, {
+            foreignKey: "professorId"
+         });
+         Person.hasMany(models.Registration, {
+            foreignKey: "studentId"
+         });
       }
-    },
-    scopes: {
-      all: {
-        where: {}
+   }
+
+   Person.init({
+      name: DataTypes.STRING,
+      active: DataTypes.BOOLEAN,
+      email: {
+         type: DataTypes.STRING,
+         validate: {
+            isEmail: {
+               args: true,
+               msg: 'invalid email'
+            }
+         }
+      },
+      role: DataTypes.STRING
+   }, {
+      sequelize,
+      modelName: 'Person',
+      paranoid: true,
+      defaultScope: {
+         where: {
+            active: true
+         }
+      },
+      scopes: {
+         all: {
+            where: {}
+         }
       }
-    }
-  });
-  return Person;
+   });
+   return Person;
 };
