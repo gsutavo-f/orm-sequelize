70769c6d4854738cc82eb8071a7e6670142e1e66
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-10 17:16:46 -0300

    [feat] adding finalDate column to Classes

--- api/models/class.js
+++ api/models/class.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Class extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Class.hasMany(models.Registration);
       Class.belongsTo(models.Person, {
@@ -20,7 +15,8 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Class.init({
-    startDate: DataTypes.DATEONLY
+    startDate: DataTypes.DATEONLY,
+    finalDate: DataTypes.DATEONLY
   }, {
     sequelize,
     modelName: 'Class',
